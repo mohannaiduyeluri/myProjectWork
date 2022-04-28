04feb82ac1c43e05caefb85017ff331bd70d86dc
mohannaiduyeluri <mohansandeep.yeluri@gmail.com>
2022-04-28 21:51:57 +0500

    contact us | fixed ui

--- client/src/pages/Contact.vue
+++ client/src/pages/Contact.vue
@@ -1,27 +1,37 @@
-<script scoped lang="ts">
-	//
+<script setup lang="ts">
+import { ref } from 'vue';
+import router from '../router';
+
+const label = ref("Submit");
+
+const submit = () => {
+	label.value = "Message Sent!";
+	setTimeout(() => {
+		router.push('./tasks');
+	}, 500);
+}
+
 </script>
 
 <template>
-	<h1 class="title">Contact Us</h1>
-
 	<div class="frm">
+		<h1 class="title">Contact Us</h1>
 		<input class="input" type="text" placeholder="Name">
 		<input class="input" type="email" placeholder="Email">
 		<textarea class="textarea" type="text" placeholder="Message"></textarea>
-		<button class="button is-success">Submit</button>
+		<button class="button is-success" @click="submit">{{ label }}</button>
 	</div>
 </template>
 
 <style scoped lang="scss">
-.title {
-	position: absolute;
-	top: 200px;
-	left: 50%;
-	transform: translateX(-50%);
-}
 
 .frm {
+	.title {
+		width: 100%;
+		text-align: center;
+		margin-bottom: 50px;
+	}
+
 	input,
 	textarea,
 	button {
